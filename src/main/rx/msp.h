b3ec1d29608eb0af4e52f945453bf6ea8a0c248a
Levi Burner <leviburner@gmail.com>
2017-07-06 11:31:35 -0400

    Add dual rx arm and disarm procedures

--- src/main/rx/msp.h
+++ src/main/rx/msp.h
@@ -21,3 +21,4 @@ struct rxConfig_s;
 struct rxRuntimeConfig_s;
 void rxMspInit(const struct rxConfig_s *rxConfig, struct rxRuntimeConfig_s *rxRuntimeConfig);
 void rxMspFrameReceive(uint16_t *frame, int channelCount);
+void rxMspChannelsReset();
